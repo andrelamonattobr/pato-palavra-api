a23fd3c51a891421c8c53a5948eb647772dbe025
andrelamonattobr <andrelamonattobr@gmail.com>
2025-05-19 19:39:22 -0300

    Agora eh torcer para os testes funcionar

--- src/test/java/com/pato_palavra/PatoPalavraApplicationTests.java
+++ src/test/java/com/pato_palavra/PatoPalavraApplicationTests.java
@@ -1,9 +1,14 @@
 package com.pato_palavra;
 
+import com.pato_palavra.config.TestDatabaseConfig;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
 
 @SpringBootTest
+@ActiveProfiles("test")
+@ContextConfiguration(classes = TestDatabaseConfig.class)
 class PatoPalavraApplicationTests {
 
 	@Test
